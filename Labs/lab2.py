1f665169806c7c1d62f61a41eb06a3dafd9fff2e
LinaM20 <c19366191@mytudublin.ie>
2020-10-03 23:44:19 +0100

    my lab2 solution

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -10,56 +10,88 @@ class Types_and_Strings:
 
     def play_with_strings(self):
         # working with strings
-        message = input("Enter your noun: ")
+        message = input("Enter your name: ")
         print("Originally entered: "+ message)
+
+
 
         #
         # Enter your own print statements below:
         #
 
         # print only first and last of the sentence:
+        print(""+message[0])
+        print(""+message[-1])
+
+
 
 
         # use slice notation:
+        print(""+message[3:])
+
 
 
         # escaping a character:
-
+        print('He said "That\'s Fantastic"')
 
         # find all a's in the input word and count how many there are:
+        y = message.find("a")
+        print(y)
+        y = message.count("a")
+        print(y)
 
 
         # replace all occurences of the character a with the - sign
+        #message[3]='-'
+        #print("" +message)
+
+
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace():
-
+        x = message.replace("a", "-")
+        print(x)
 
     def play_with_lists(self):
         message = input("Please enter a whole sentence: ")
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out:
-
+        my_list = list(message.split(" "))
+        print(my_list)
 
         # append a new element to the list and print:
+        my_list.append("Love")
+        print(my_list)
 
 
         # remove from the list in 3 ways:
+        #my_list.pop(-1)
+        #print(my_list)
+
+        #my_list.remove('Love')
+        #print(my_list)
+
+        #my_list.clear()
+        #print(my_list)
+
 
 
         # check if the word cake is in your input list:
-
+        print("cake" in my_list)
 
         # reverse the items in the list and print:
-
+        #my_list.reverse()
+        #print(my_list)
 
         # reverse the list with the slicing trick:
+        print(my_list[::-1])
 
 
-        # print the list 3 times by using multiplication:
+        # print the list 3 times by using multiplication
+        print((my_list)*3)
 
 
 
 tas = Types_and_Strings()
-tas.play_with_strings()
-#tas.play_with_lists()
+#tas.play_with_strings()
+tas.play_with_lists()
